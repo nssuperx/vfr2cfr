da72e9821784c926229d55731cdd43a56f4021b8
nssuperx <mgtmgtx@gmail.com>
2020-03-13 23:20:04 +0900

    リファクタリング

--- vfr2cfr/FormMain.cs
+++ vfr2cfr/FormMain.cs
@@ -53,55 +53,6 @@ namespace vfr2cfr
             //入力できるようにする
             p.StartInfo.RedirectStandardInput = true;
 
-            //非同期で出力を読み取れるようにする
-            //p.StartInfo.RedirectStandardOutput = true;
-            //p.OutputDataReceived += p_OutputDataReceived;
-
-
-            p.StartInfo.Arguments = @"/c ffmpeg -h";
-
-            //起動
-            p.Start();
-
-            //非同期で出力の読み取りを開始
-            //p.BeginOutputReadLine();
-            //出力を読み取る
-            string results = p.StandardOutput.ReadToEnd();
-            /*
-            //入力のストリームを取得
-            System.IO.StreamWriter sw = p.StandardInput;
-            if (sw.BaseStream.CanWrite)
-            {
-                foreach (string outFilePath in outFilePaths)
-                {
-                    string f = outFilePath;
-                    if (Path.GetExtension(f) == ".avi")
-                    {
-                        f = Path.Combine(Path.GetDirectoryName(f), Path.GetFileNameWithoutExtension(f) + "-out");
-                    }
-                    f = Path.ChangeExtension(f, "avi");
-
-                    //sw.WriteLine(@"ffmpeg -i " + "\"" + outFilePath + "\"" + " -r 60 -vsync cfr -af aresample=async=1 -vcodec utvideo -acodec pcm_s16le " + "\"" + f + "\"");
-                    sw.WriteLine(@"ffmpeg -h");
-                    
-                    //プロセス終了まで待機する
-                    //WaitForExitはReadToEndの後である必要がある
-                    //(親プロセス、子プロセスでブロック防止のため)
-                    //p.WaitForExit();
-
-
-                    
-                }
-            }
-            sw.Close();
-            */
-
-            //Console.ReadLine();
-            p.WaitForExit();
-            p.Close();
-            //出力された結果を表示
-            Console.WriteLine("1"+results);
-
             foreach (string outFilePath in outFilePaths)
             {
                 string f = outFilePath;
@@ -113,28 +64,10 @@ namespace vfr2cfr
 
                 p.StartInfo.Arguments = @"/c ffmpeg -i " + "\"" + outFilePath + "\"" + " -r 60 -vsync cfr -af aresample=async=1 -vcodec utvideo -acodec pcm_s16le " + "\"" + f + "\"";
                 p.Start();
-                //results = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
                 p.Close();
                 Console.WriteLine("output file: " + Path.GetFileName(f));
             }
-
-            /*
-            p.StartInfo.Arguments = @"/c ffmpeg -h";
-            p.Start();
-            results = p.StandardOutput.ReadToEnd();
-            p.WaitForExit();
-            p.Close();
-            Console.WriteLine("2"+results);
-            */
-
-        }
-        //OutputDataReceivedイベントハンドラ
-        //行が出力されるたびに呼び出される
-        static void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
-        {
-            //出力された文字列を表示する
-            Console.WriteLine(e.Data);
         }
     }
 }
