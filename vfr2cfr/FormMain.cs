7e954205534508e3e36820fa1055a0c0a941c28f
nssuperx <mgtmgtx@gmail.com>
2020-03-13 18:37:41 +0900

    もう少しのところまできた

--- vfr2cfr/FormMain.cs
+++ vfr2cfr/FormMain.cs
@@ -39,22 +39,30 @@ namespace vfr2cfr
 
         private void OutButton_Click(object sender, EventArgs e)
         {
+            //参考ページ:https://dobon.net/vb/dotnet/process/standardoutput.html
             //Processオブジェクトを作成
             System.Diagnostics.Process p = new System.Diagnostics.Process();
             //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
             p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+            p.StartInfo.CreateNoWindow = true;
+
             //出力を読み取れるようにする
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
+
             //入力できるようにする
             p.StartInfo.RedirectStandardInput = true;
+
             //非同期で出力を読み取れるようにする
             p.StartInfo.RedirectStandardOutput = true;
             p.OutputDataReceived += p_OutputDataReceived;
+
             //起動
             p.Start();
+
             //非同期で出力の読み取りを開始
             p.BeginOutputReadLine();
+
             //入力のストリームを取得
             System.IO.StreamWriter sw = p.StandardInput;
             if (sw.BaseStream.CanWrite)
@@ -67,18 +75,8 @@ namespace vfr2cfr
                         f = Path.Combine(Path.GetDirectoryName(f), Path.GetFileNameWithoutExtension(f) + "-out");
                     }
                     f = Path.ChangeExtension(f, "avi");
-                    //Console.WriteLine(f);
-
-                    //コマンドラインを指定（"/c"は実行後閉じるために必要）
-                    //p.StartInfo.Arguments = @"dir c:\ /w";
-
                 
-                    //「dir c:\ /w」を実行する
-                    sw.WriteLine(@"echo " + f);
-                    //終了する
-                    //sw.WriteLine("exit");
-
-                    
+                    sw.WriteLine(@"ffmpeg -i " + "\"" + outFilePath + "\"" + " -r 60 -vsync cfr -af aresample=async=1 -vcodec utvideo -acodec pcm_s16le " + "\"" + f + "\"");
 
                     //出力を読み取る
                     //string results = p.StandardOutput.ReadToEnd();
@@ -92,17 +90,16 @@ namespace vfr2cfr
                     //出力された結果を表示
                     //Console.WriteLine(results);
                 }
-                
             }
             sw.Close();
+            //Console.ReadLine();
             p.WaitForExit();
             p.Close();
-            Console.ReadLine();
+            
         }
         //OutputDataReceivedイベントハンドラ
         //行が出力されるたびに呼び出される
-        static void p_OutputDataReceived(object sender,
-            System.Diagnostics.DataReceivedEventArgs e)
+        static void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
         {
             //出力された文字列を表示する
             Console.WriteLine(e.Data);
